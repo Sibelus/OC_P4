03d0515df46b58931f5f5a6319b5cde308f93336
Gaëtan <gaetangivry@gmail.com>
2022-10-19 11:56:43 +0200

    Creation of two integration test - TestParkingACar & TestParkingLotExit

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +15,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,17 +56,39 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+        // GIVEN
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
+
+        // WHEN
+        Ticket returnedTicket = ticketDAO.getTicket("ABCDEF");
+        int nextAvailableParkingSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        // THEN
+        assertEquals(returnedTicket.getVehicleRegNumber(), "ABCDEF");
+        assertEquals(nextAvailableParkingSlot, 2);
     }
 
     @Test
     public void testParkingLotExit(){
+        // GIVEN
         testParkingACar();
+
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+
+        // WHEN
+        Ticket returnedTicket = ticketDAO.getTicket("ABCDEF");
+        Date outTimeTicket = returnedTicket.getOutTime();
+
+        Date outTime = new Date();
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        // THEN
+        assertEquals(returnedTicket.getPrice(), 0);
+        assertEquals(dt.format(outTime), dt.format(outTimeTicket));
     }
 
 }
